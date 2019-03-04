21309802911201afce21c12542f0b09b560ed4e6
Simon Tharby <jinjamail@gmail.com>
2019-03-04 16:28:31 +0000

    bugfix: form_for moved to correct html level (now form functions after navigated to via link)

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,20 +2,14 @@ class FlightsController < ApplicationController
 
   def index
     unless params[:find_flights].nil?
-      puts "params: #{params}"
       @flights_found = Flight.where(from_id: params[:find_flights][:from],
                                     to_id: params[:find_flights][:to]).all
-      @flights_found.each { |f| p f}
+      @passenger_count = params[:find_flights][:passenger_count]
+      @date = params[:find_flights][:date]
     end
   end
 
   def list
     @flights = Flight.all
   end
-
-  private
-
-    def user_params
-      params.require(:find_flights).permit(:from, :to)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'flights#index'
-  get    '/list',  to: 'flights#list'
+  get  '/list',to: 'flights#list'
   resources :flights
 end
